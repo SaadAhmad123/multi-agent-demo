022688b06ed5f53cf8451bae6354c459e7019169
Saad Ahmad <saadkwi12@hotmail.com>
2025-11-18 09:55:01 +1100

    update

--- packages/agentic-handlers/src/handlers/orchestrator.demo/machine.V100.ts
+++ packages/agentic-handlers/src/handlers/orchestrator.demo/machine.V100.ts
@@ -24,7 +24,7 @@ export const demoMachineV100 = setupArvoMachine({
     },
   },
 }).createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5QTAWwPYFkCGBjAFgJYB2YAagIwAMVAdPgK6rbECCADuwE7oBu2AGwDEYXgBd6TFrWyce-AbVgNcuOLADaVALqJQ7dLEJjC6YnpAAPRAA4atGjQDMVAKwUA7K482KAGhAAT1sKWlcAX3CAlAwcAhJyajpGZjY5PkERcUlUmXSFJRU1WE0KXSQQAyMTMwtrBABODwDghAonADYAFlonVwanL0jotCw8IlJKexSWDm4M4VhA2FpcdFQc6Vl5grAuHi4tcv1DY1NzCvq7OkcqF3cvH38gxCcAJidhkBix+Mmk1aCXAMATYGrEIQQMxgJRiMEwn5xCaJey4IEgsHnI4WKpnWqXWz2W73TzeXwtWw2Dq0BpvBodGxDKLfUZIhJTOhogTA0HgmQQCB44i0MCWMDAsRgLISLk8sHoLj8iCFVTqbEVXHguqvLquWhdGxdQauCltKhNfVUDpvCLMxHjdkA2UYvnYAVCkViiVSpYrNYbZ28hVKkX7BXqk7Vc7ahDXByOEmPckvM0uXo2N6+G1fe1-FGc9G8860VAgkzsASENF80XihiS6WA7ku4PzCAqCTKVUlCOVU5agkIJwUPX0xkdVw2xk2BpW00eWc01zD7N21kO-6owuYswlsuECtVnfC2veoS+1brJtysStnjt3ASPYHXua6ODuPEtykp6mgY9Lo6QZJlmWIdAUHgCpc2RDkcX7d9QHqdpTQAWg6HN1zzDlNjSHZBDgqN8UQxBrWpLpgInKcfAaZ5Wk8BpaFXEZYg3fNrxdBC+0Ii5iIQDpaJIkclxXW1mN+GCnW3V13QHSMhRjdobDCEcOnNDwugoN5qAaLpTSSBjZ2tUSWRYrDJObItdzdQUay9eswAI+TBwoKllNcVSmg0rTzV0lMKH80InAzLNjOgx0tws48lQ9KFSEc2SrBIu5ei8KgRyomcBLafyGKCzNNNCzCJIim9i1LARy0ratOLfIjErNBp52S3URIw0zioLSK+XKyqj1sutJXizikO8U07B6WkxyZMS2U3TrSt3HqDyqqLYocjV4Lq+pVKcFLXDSydXGnGixs0mkgMZQr2vCug1qGrbEH8jwPAcJ5XHen9k1aGxDVoDoF2Aq7xJu0MDnunj6qel6qDej6kyypo6DeK1V0iIA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QTAWwPYFkCGBjAFgJYB2YAagIwAMVAdPgK6rbECCADuwE7oBu2AGwDEYXgBd6TFrWyce-AbVgNcuOLADaVALqJQ7dLEJjC6YnpAAPRAFoAzAHYAHLSdOATJ-dUALAE5-AFZAhwAaEABPRD86Jz8ANgpAmioKJ0CPOwBfLPCUDBwCEnJqOkZmNjk+QRFxSQqZKoUlFTVYTQpdJBADIxMzC2sEezs7Wnc-PydqCjs4jPd48KiEH3iXCniQ9yTUu0nAnLy0LDwiUkoaepYObmrhWAjYWlx0VGviRrvmsC4eLi0XX0hmMpnM3SGNni7jGgUmdgyqUSU2WiB8TjGfiSDh2FACdi2fiOIHypyKF1KL0EuAYAmw-WIQggZjASjE9NZpMK5xKV1w1Np9LBgIsvVBAwhiAo7gcPloAVmfgRjji8SWkUQjjlWwoiuCdioTgcauJXLOxUudH5AhpdIZMggEHFnzAljANLEYFqEmttvp6C4DogLVU6hF3TFDMGiAcBNosbx8TmgS2-jsqIQIUC8eNgQJiVmHlNJ25FspvsF9uwjudtFd7oYnqEj2er3eFbtAaDdb+AfDwL6YOjCANLl8E32+xhPgyGecdACgRh8R8-jViWLBXNFL5ArtYNoqFpJnYAkI-Pt9Y9XtEPr3-sDdwgKgkylD7X7PRBUclCAcVAccZNgRHxdTzOYfAzRY-FoOxp11As5jxBxNzJHlLSpG1KwPI8BBPM8LwPK9Gy9FsXjeTC-TELsnxfHt-k-SMh1-AJxgcUofH-Ti4TiDN0UxbFcXxQkclyEBiHQFB4G6M1yV5KhRW-ZjQEheJ2NoOElURTY8ScDMxn2RJOIxOI1iTJVUNLHcyikSpvkERTBwlFTbBTMd1l1Hx3FAgl3ECfSqGzOD0SXOJZRXNTLO3eTKOw5yvyc8EXNWJdxm4tx0lTJUM1YuC7B8KhEmcRxdSiuSMI7IUzCDZ1HNq38lxg407F0uEHGNKh0w1VY3DYrZ2o8QJdSSbIxNk9Dy3vKsa0vN1rzqn9kpCQDmtavx2viTqMwyJq8yoSZhrmdwyom3csP3arqyde1mVIBblKsaJWOhRZfDA0YnEg7q2vjPaDt1I6TrLM6qJw49CFPc8qqShL6qWq5vIcDI3BTQI0wzNIKFcadDVMo0VycIHrNii7Plw-CodmhtPXu+KhjRlxEeRzK0ey7r3AxcZJnW0CAemdaiZiyr7XJiGCOh2hbrAWmYaGSZtU8TbefAz6oKxLnJllQ7+ZQsaS2ijCpZl4cKFXQD-CSPwZXynwWr07qSvlVI8TWJVpniCy9a3crKV+f5jd-Gw0fiDTNpat6tNSDMjToNINqNU2KBxUaciAA */
   id: 'demoMachineV100',
   // Populate the initial context of the state machine
   context: ({ input }) => ({
@@ -48,9 +48,9 @@ export const demoMachineV100 = setupArvoMachine({
     return {
       errors: null,
       sum: context.sum,
-      product: context.sum,
+      product: context.product,
       success: true,
-      result: context.sum,
+      result: context.sum === null || context.product === null ? null : context.sum / context.product,
     };
   },
   // The initial state of the machine
@@ -85,12 +85,12 @@ export const demoMachineV100 = setupArvoMachine({
                 },
               ],
             }),
-            target: 'error'
+            target: 'error',
           },
         ],
         'sys.com.human.approval.error': {
-          actions: xstate.assign({errors: ({context, event}) => [...context.errors, event.data]}),
-          target: 'error'
+          actions: xstate.assign({ errors: ({ context, event }) => [...context.errors, event.data] }),
+          target: 'error',
         },
       },
     },
